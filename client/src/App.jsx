c562ea06e31cd250e094a3d130ea548a148918ee
Angela del Pan <angeladelpan@gmail.com>
2025-02-25 15:56:39 +0100

    register page v2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,36 +1,41 @@
 // Importamos los componentes.
 import { Routes, Route } from 'react-router-dom';
-//import { Toaster } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 // Importamos las páginas.
+import RegisterPage from './pages/RegisterPage';
+import HomePage from './pages/HomePage';
 
 const App = () => {
     return (
         <>
             <Header />
+
+            <Toaster position="top-center" />
+
             {/* Todos los endpoints han de definirse dentro de este compontente. */}
             <Routes>
-                <Route path='/' element={<HomePage />} />
-                <Route path='/register' element={<RegisterPage />} />
-                <Route path='/login' element={<LoginPage />} />
-                <Route
-                    path='/users/validate/:regCode'
+                <Route path="/" element={<HomePage />} />
+                <Route path="/register" element={<RegisterPage />} />
+                {/*<Route path="/login" element={<LoginPage />} />*/}
+                {/*<Route
+                    path="/users/validate/:regCode"
                     element={<ValidateUserPage />}
-                />
-                <Route path='/users/profile' element={<UserProfilePage />} />
-                <Route
-                    path='/users/password/recovery/request'
+                />*/}
+                {/*<Route path="/users/profile" element={<UserProfilePage />} />*/}
+                {/*<Route
+                    path="/users/password/recovery/request"
                     element={<SendRecoveryPassPage />}
-                />
-                <Route
-                    path='/users/password/:recoverPassCode'
+                />*/}
+                {/*<Route
+                    path="/users/password/:recoverPassCode"
                     element={<UseRecoveryPassCode />}
-                />
+                />*/}
                 {/* Añadir más rutas de endpoints. */}
 
-                <Route path='*' element={<NotFoundPage />} />
+                {/*<Route path="*" element={<NotFoundPage />} />*/}
             </Routes>
 
             <Footer />
---
+++ client/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                'light-blue': '#e5f7ff',
+                'dark-blue': '#083059',
+                'medium-blue': '#179dd9',
+                'accent-blue': '#3951aa',
+            },
+        },
+    },
+    plugins: [],
+};
